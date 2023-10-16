0f50c04cd84a6e7b031b12686f42acd2d2f9ed87
JaiTheGuy <31665724+JaiTheGuy@users.noreply.github.com>
2023-10-15 23:16:14 -0400

    Create loopRangeExercise.py

---
+++ loopRangeExercise.py
@@ -0,0 +1,47 @@
+# Pattern 1:
+# 21, 25, 29, ... 97
+########### CODE BELOW HERE #########
+
+
+
+
+
+
+# Pattern 2:
+# -4, -2, 0, ... 3
+########### CODE BELOW HERE #########
+
+
+
+
+
+
+
+
+# Pattern 3:
+# 3 mississippi, 2 mississippi, 1 mississippi
+########### CODE BELOW HERE #########
+
+
+
+
+
+
+
+
+# Pattern 4:
+# Create a list of 5 emojis of your choice and print them using a for loop
+# 😬😎😁👽🥳
+########### CODE BELOW HERE #########
+
+
+
+
+
+
+
+
+
+
+
+
